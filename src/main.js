c75e6664e0ba0cbf50b5a2be0baacb7d9ebbc035
Silence-dream <1428482231@qq.com>
2020-12-17 17:19:43 +0800

    :fix:  修复css样式初始化

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Element3 from "element3";
 import "element3/lib/theme-chalk/index.css";
 
 // 引入样式清除
-import "@/styles/mixnins/_mixnin.scss";
+import "@/styles/common.scss";
 // 引入字体图标
 import "@/fonts/iconfont.css";
 createApp(App)

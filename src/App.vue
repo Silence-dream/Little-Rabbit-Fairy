1c9af90e8a8cb119a1d2a288464a5ab89fee02a8
liuzhiyan5 <1172258408@qq.com>
2020-12-17 16:46:52 +0800

    :new: element3包安装

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   <router-view />
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  // color: $warning;
   // 请查看public文件夹里面的public/css/mixnins/_mixnin.scss
   // 定义的方法 现在里面有 清除浮动 多行省略号 单行省略号 可以在全局引用
   @include clearfix();
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
+import Element3 from "element3";
+import "element3/lib/theme-chalk/index.css";
 
 // 引入样式清除
 import "../public/css/common.scss";
 createApp(App)
   .use(store)
   .use(router)
+  .use(Element3)
   .mount("#app");

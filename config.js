3dbf3b14b96577cbc65c5d7e803e1b733ea15600
Silence-dream <1428482231@qq.com>
2020-12-18 15:39:29 +0800

    API 提交测试

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 export const baseHost = {
   API1: {
     // 开发的基准地址
-    dev_host: "https://mock.boxuegu.com/mock/1175/"
+    dev_host: "https://mock.boxuegu.com/mock/1175/",
+    pro_host: "https://mock.boxuegu.com/mock/1175/"
   }
 };

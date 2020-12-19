5fd7fa2488aaae3af444327f76e44f3f1503aaa9
Silence-dream <1428482231@qq.com>
2020-12-19 23:27:50 +0800

    :new: 修改: eslint 规范代码规则添加,并修复了不符合规则的写法

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-implicit-coercion": "error", //禁止隐式转换,
+    camelcase: "error", //强制驼峰法命名
+    "no-else-return": 2, //如果if语句里面有return,后面不能跟else语句
   }
 };

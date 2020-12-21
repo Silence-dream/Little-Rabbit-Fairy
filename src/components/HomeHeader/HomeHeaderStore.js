ecf7943fbfef2ff44b4595c2e0cd057ee6c38532
liuzhiyan5 <1172258408@qq.com>
2020-12-21 20:34:45 +0800

    :tada:首页头部组件渲染完成

---
+++ src/components/HomeHeader/HomeHeaderStore.js
@@ -0,0 +1,19 @@
+const state = {
+  homeCategory: []
+};
+const mutations = {
+  liveCategory(state, val) {
+    state.homeCategory = val;
+    console.log(state.homeCategory);
+  }
+};
+const actions = {};
+const getters = {};
+
+// 不要忘记把state, mutations等暴露出去。
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+};
---
+++ src/components/HomeLogo/HomeLogo.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="container">
+    <el-row>
+      <el-col :span="24">
+        <!-- 顶部logo行 -->
+        <div class="logo-nav">
+          <!-- logo -->
+          <div class="logo">
+            <img src="./img/rabbitLogo.png" class="rabbit_logo" />
+          </div>
+
+          <!-- 头部导航栏 -->
+          <ul>
+            <li>
+              <a href="javascript:;" class="active">首页</a>
+            </li>
+            <li>
+              <a href="javascript:;">美食</a>
+            </li>
+            <li>
+              <a href="javascript:;">餐厨</a>
+            </li>
+            <li>
+              <a href="javascript:;">艺术</a>
+            </li>
+            <li>
+              <a href="javascript:;">电器</a>
+            </li>
+            <li>
+              <a href="javascript:;">居家</a>
+            </li>
+            <li>
+              <a href="javascript:;">洗护</a>
+            </li>
+            <li>
+              <a href="javascript:;">孕婴</a>
+            </li>
+            <li>
+              <a href="javascript:;">服装</a>
+            </li>
+            <li>
+              <a href="javascript:;">杂货</a>
+            </li>
+          </ul>
+
+          <!-- 搜索框 -->
+          <div class="search">
+            <div class="search-in" :class="{ active: active }">
+              <i class="iconfont icon-sousuo1 searchIcon"></i>
+              <el-input
+                v-model="searchInput"
+                placeholder="搜一搜"
+                clearable
+                style="display: inline-block; border:none;"
+                @focus="focusSearchBorder()"
+                @blur="focusSearchBorder()"
+              />
+            </div>
+          </div>
+
+          <!-- 购物车 -->
+          <div class="shopping-cart">
+            <el-badge :value="12" class="item">
+              <i class="iconfont icon-gouwuchekong"></i>
+            </el-badge>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { httpGet } from "@/utils/http";
+import { useStore } from "vuex";
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const Store = useStore();
+    // 搜索框文本
+    const searchInput = ref("");
+    // 搜索框样式
+    let active = ref(false);
+
+    httpGet("home/category/head")
+      .then(res => {
+        // console.log(res);
+        const oLiData = res.result;
+        // 存入vuex中
+        Store.commit("liveCategory", oLiData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    // 点击搜索框边框效果
+    const focusSearchBorder = function() {
+      active.value = !active.value;
+      // console.log(active.value);
+    };
+
+    return {
+      active,
+      searchInput,
+      focusSearchBorder
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+// 顶部logo行
+.logo-nav {
+  height: 130px;
+  // background-color: pink;
+  overflow: hidden;
+
+  // logo
+  .logo {
+    display: flex;
+    float: left;
+    width: 205px;
+    height: 100%;
+    align-items: center;
+
+    .rabbit_logo {
+      width: 100%;
+      height: 70px;
+    }
+  }
+
+  // 头部导航栏
+  ul {
+    display: flex;
+    float: left;
+    height: 100%;
+    align-items: center;
+    margin-left: 12px;
+    margin-right: 34px;
+
+    .active {
+      color: #27ba9b;
+    }
+
+    li {
+      font-size: 16px;
+      font-weight: 700;
+      color: #333;
+      margin: 0 2px;
+
+      a {
+        display: inline-block;
+        padding: 20px 14px;
+      }
+
+      &:hover {
+        a {
+          color: #27ba9b;
+          border-bottom: 2px solid #27ba9b;
+        }
+      }
+    }
+  }
+
+  // 搜索框
+  .search {
+    float: left;
+    display: flex;
+    height: 100%;
+    align-items: center;
+
+    .search-in {
+      position: relative;
+      border-bottom: 1px solid #e7e7e7;
+      padding-left: 10px;
+
+      .searchIcon {
+        position: absolute;
+        top: 9px;
+        left: -2px;
+        font-size: 18px;
+        // font-weight: 700;
+        z-index: 1;
+      }
+    }
+
+    .active {
+      border-bottom: 1px solid #1abc9c;
+    }
+  }
+
+  // 购物车
+  .shopping-cart {
+    float: left;
+    display: flex;
+    height: 100%;
+    align-items: center;
+
+    .item {
+      margin-left: 24px;
+
+      .icon-gouwuchekong {
+        font-size: 22px;
+      }
+    }
+  }
+}
+</style>

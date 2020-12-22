32264d5a8acd5509d24d09dc6fc91ccb00d1b197
Silence-dream <1428482231@qq.com>
2020-12-22 18:15:49 +0800

    :new:  登录页面样式完成

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -3,17 +3,21 @@
   <LoginHeader></LoginHeader>
   <!-- 登录界面组件 -->
   <LoginBanner></LoginBanner>
+  <!-- 公共底部组件  -->
+  <LoginFooter></LoginFooter>
 </template>
 
 <script>
 import LoginHeader from "@/components/LoginHeader/LoginHeader";
 import LoginBanner from "@/components/LoginBanner/LoginBanner.vue";
+import LoginFooter from "@/components/LoginFooter/LoginFooter.vue";
 export default {
   name: "Login",
   setup() {},
   components: {
     LoginHeader,
-    LoginBanner
+    LoginBanner,
+    LoginFooter
   }
 };
 </script>

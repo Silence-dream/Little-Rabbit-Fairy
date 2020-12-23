7172b1a3060254e0be64c39bb4664be678cb18b8
Silence-dream <1428482231@qq.com>
2020-12-23 20:34:59 +0800

    :new: 提交:注册组件样式完成

--- src/components/RegisterSuccess/RegisterSuccess.vue
+++ src/components/RegisterSuccess/RegisterSuccess.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="register-success"></div>
+  <div class="register-success">
+    <div class="success-box">
+      <img src="./img/right.png" alt="图片失效了" />
+      <h1>恭喜注册成功</h1>
+      <span>请妥善保管您的账户信息</span>
+      <div class="btn">
+        <el-button>
+          <router-link to="/">查看购物车</router-link>
+        </el-button>
+        <el-button>
+          <router-link to="/">进入首页</router-link>
+        </el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +22,41 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.register-success {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 600px;
+  h1 {
+    color: #333333;
+    font-size: 18px;
+    padding: {
+      top: 20px;
+    }
+  }
+  span {
+    display: block;
+    color: #999999;
+    font-size: 16px;
+    padding: 30px 0;
+  }
+}
+/* 覆盖element样式 */
+::v-deep .btn {
+  .el-button {
+    width: 180px;
+    height: 50px;
+    border: none;
+    a {
+      color: #ffffff;
+    }
+    &:nth-child(1) {
+      background-color: #27ba9b;
+    }
+    &:nth-child(2) {
+      background-color: #cccccc;
+    }
+  }
+}
+</style>

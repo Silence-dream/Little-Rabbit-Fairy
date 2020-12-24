5c65dc6b90c9823401b3030fe70d0cd88a736d07
Silence-dream <1428482231@qq.com>
2020-12-24 15:56:39 +0800

    :new: 提交: 注册页面接口调用,注册成功页面样式修改

--- src/api/index.js
+++ src/api/index.js
@@ -16,5 +16,6 @@ export const common = {
 // 公共分类
 export const common = {
   Login: "login", // 账号登录
-  LoginCode: "login/code" // 手机验证码登录
+  LoginCode: "login/code", // 手机验证码登录
+  Regiser: "regiser"
 };

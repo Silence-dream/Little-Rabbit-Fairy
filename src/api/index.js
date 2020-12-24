ac25f37fe697f6555498fcb035e1c880fff911ce
Silence-dream <1428482231@qq.com>
2020-12-24 15:18:34 +0800

    :new: 提交: 账号登录和验证码登录接口调用

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,9 @@ export const home = {
   GetLogoNav: "home/category", // logo导航栏
   GetFreshGood: "home/new" // 新鲜好物
 };
+
+// 公共分类
+export const common = {
+  Login: "login", // 账号登录
+  LoginCode: "login/code" // 手机验证码登录
+};
--- src/components/HomeHeader/HomeHeader.vue
+++ src/components/HomeHeader/HomeHeader.vue
@@ -12,7 +12,9 @@
                   <p>
                     <router-link to="/login" class="sign">请先登录</router-link>
                     <span>|</span>
-                    <router-link to="#" title="免费注册">免费注册</router-link>
+                    <router-link to="/register" title="免费注册"
+                      >免费注册</router-link
+                    >
                     <span>|</span>
                     <router-link to="#" title="我的订单">我的订单</router-link>
                     <span>|</span>

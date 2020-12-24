9b7e52c25b23db3df62d9c97823a1b05d22b58e7
liuzhiyan5 <1172258408@qq.com>
2020-12-24 15:40:25 +0800

    :new:热门品牌组件完成

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,6 @@
 export const home = {
   GetLogoNav: "home/category", // logo导航栏
   GetFreshGood: "home/new", // 新鲜好物
-  GetPopularityRecommend: "home/hot" // 人气推荐
+  GetPopularityRecommend: "home/hot", // 人气推荐
+  GetHotBrands: "home/brand" // 热门品牌
 };

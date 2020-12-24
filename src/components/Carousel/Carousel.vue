2bd17d89e07eddc6e6c8557c3f7d20ce9e1175f2
Silence-dream <1428482231@qq.com>
2020-12-24 20:06:54 +0800

    :bug: 修复:#50

--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -26,11 +26,6 @@ import { Swiper, SwiperSlide } from "swiper/vue";
 // import Swiper core and required components
 import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from "swiper";
 
-// Import Swiper styles
-import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss";
-import "swiper/components/pagination/pagination.scss";
-
 // install Swiper components
 SwiperCore.use([Navigation, Pagination, A11y, Autoplay]);
 export default {
@@ -64,4 +59,4 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
